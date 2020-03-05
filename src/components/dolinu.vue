f69d51b61af2c253a03bcc09aede72b27bcc5b86
edyMartin2 <61470326+edyMartin2@users.noreply.github.com>
2020-03-05 12:47:01 -0600

    v2.0.3

---
+++ src/components/dolinu.vue
@@ -0,0 +1,69 @@
+<template>
+     <div class="menu">
+      <img
+        src="http://insyc.southcentralus.cloudapp.azure.com/viewimage.php?cache=1&modulepart=mycompany&file=logos%2Fthumbs%2FSDC+SLOGAN_mini.jpg"
+        style="margin-left: 2em; margin-top:2em;margin-bottom:2em"
+      />
+      <input
+        list="items"
+        style="width:100%; background:rgba(255,255,255,0); border-style:none; margin-bottom:1em"
+        placeholder="Buscar"
+      />
+      <datalist id="items">
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+      </datalist>
+      <input
+        list="mark"
+        style="width:100%; background:rgba(255,255,255,0); border-style:none; margin-bottom:1em"
+        placeholder="Marcadores"
+      />
+      <datalist>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+      </datalist>
+      <br>
+      <i class="typcn typcn-home-outline  icons"> Citados hoy</i> <br>
+      <hr>
+      <i class="typcn typcn-pin-outline icons"> Citas pendientes</i>
+      <hr>
+      <i class="typcn typcn-coffee icons"> Historial</i>
+      <hr>
+      <i class="typcn typcn-folder-add icons"> Añadir</i>
+      <hr>
+    </div>
+</template>
+<script>
+export default {
+  name:'dolinu'
+}
+</script>
+<style  scoped>
+.menu {
+  background-color: #ededed;
+  width: 15em;
+  height: 100vh;
+  float: left;
+  margin: 0;
+  padding: 0;
+}
+.menu-list {
+  list-style: none;
+}
+.menu-list > li {
+  cursor: pointer;
+}
+.menu > span{
+  cursor: pointer;
+}
+.icons{
+  white-space: nowrap;
+  font-family: roboto,arial,tahoma,verdana,helvetica;
+  text-align: left;
+  font-weight: bold;
+  cursor:pointer;
+  font-size: 1.1em;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import 'jquery'
 import 'typicons.font/src/font/typicons.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import store from './store'
+import router from './router'
 
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
